dbd03533cc4aa6feadce50bf551a90f1e2243409
Maciek <maciejchamera2003@gmail.com>
2024-04-06 13:24:21 +0200

    Initial commit Changes to be committed: 	new file:   pom.xml 	new file:   src/main/java/com/ocado/BasketSplitter.java 	new file:   target/classes/com/ocado/BasketSplitter.class

---
+++ src/main/java/com/ocado/BasketSplitter.java
@@ -0,0 +1,16 @@
+package com.ocado;
+
+import java.util.List;
+import java.util.Map;
+
+public class BasketSplitter {
+    public BasketSplitter(String absolutePathToConfigFile) {
+        /* ... */
+    }
+
+    public Map<String, List<String>> split(List<String> items) {
+        /* ... */
+        return null;
+    }
+    /* ... */
+}
